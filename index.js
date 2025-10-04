55e71cda6d578fbb5ca81e5d9152c1e4f273948b
Dawn <dev.dbosing@gmail.com>
2025-10-04 16:31:58 -0600

    update index.js
    updated index.js to achieve given tests from the program

--- index.js
+++ index.js
@@ -20,8 +20,10 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', (req, res) => {
+  res.json({ipaddress: req.ip, language: req.headers['accept-language'], software: req.headers['user-agent']});
+  res.json({language: req.headers['accept-language']});
+  res.json({software: req.headers['user-agent']});
 });
 
 // listen for requests :)
